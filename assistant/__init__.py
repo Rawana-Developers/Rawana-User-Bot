41477c9298945c1afec3c585340c656820a96ccb
SAVINDU DESHAN <92288786+SAVINDU-DESHAN@users.noreply.github.com>
2022-03-13 09:58:21 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,16 +1,16 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2022 TeamUltroid
+# Rawana - UserBot
+# Copyright (C) 2021-2022 TeamRawana
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/Rawana-Developers/Rawana-User-Bot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://www.github.com/Rawana-Developers/Rawana-User-Bot/blob/main/LICENSE/>.
 
-from pyUltroid import *
-from pyUltroid import _ult_cache
-from pyUltroid.functions.helper import *
-from pyUltroid.functions.tools import get_stored_file
-from pyUltroid.misc import owner_and_sudos
-from pyUltroid.misc._assistant import asst_cmd, callback, in_pattern
+from pyRawana import *
+from pyRawana import _ult_cache
+from pyRawana.functions.helper import *
+from pyRawana.functions.tools import get_stored_file
+from pyRawana.misc import owner_and_sudos
+from pyRawana.misc._assistant import asst_cmd, callback, in_pattern
 from telethon import Button, custom
 
 from plugins import ATRA_COL
